2c7184394129b996d2f623b9eca025334be7bd44
Margo Schaedel <margoschaedel@gmail.com>
2017-11-02 10:24:02 -0600

    Run seed files and set up knex environment in server file

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
-// const environment = process.env.NODE_ENV || 'development';
-// const configuration = require('./knexfile')[environment];
-// const database = require('knex')(configuration);
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
